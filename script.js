7875d05a7924740ab3c912e2c83fa283baec3703
Arash Ghafoori <arashinhoghafoori@gmail.com>
2022-07-13 18:19:30 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+function generateTarget(){
+    return Math.floor(Math.random()*10);
+}
+
+function compareGuesses(human, computer, target){
+    if (Math.abs(target - human) <= Math.abs(target - computer)){
+        return true;
+    }
+    return false;
+}
+
+function updateScore(winner){
+    if(winner === "human"){
+        humanScore++;
+    }
+    else if(winner === "computer"){
+        computerScore++;
+    }
+}
+
+function advancedRound(){
+    currentRoundNumber++;
+}
